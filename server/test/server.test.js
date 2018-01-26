3cba3cc443d77f09e2686518c12b3377cc647be1
Simon Zheng <zhengzt@gmail.com>
2018-01-25 22:08:35 -0500

    Add test GET /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,12 +3,14 @@
 
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
-  text: 'First text todo'
+  text: 'First text todo',
+  _id: new ObjectID('5a6949273b85e130e88d2570')
 },{
   text: 'Second test todo'
 }];
@@ -80,4 +82,34 @@ describe('GET /todos', () => {
       })
       .end(done);
   });
+});
+
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get('/todos/'+hexId)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+
+
 });
