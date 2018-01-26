918ecc1ceabd96f3e54b36feeab03c1e385a9441
Simon Zheng <zhengzt@gmail.com>
2018-01-25 21:43:45 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,23 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 })
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send(id + ' not valid');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send(id + ' not found');
+    }
+
+    res.status(200).send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
