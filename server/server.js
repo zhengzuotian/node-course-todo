5d0c6da849c13a0f67b270bae601579358d34752
Simon Zheng <zhengzt@gmail.com>
2018-01-27 19:53:57 -0500

    GET /

--- server/server.js
+++ server/server.js
@@ -21,13 +21,17 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome');
+});
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send(todos);
   }, (e) => {
     res.status(400).send(e);
   });
-})
+});
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
